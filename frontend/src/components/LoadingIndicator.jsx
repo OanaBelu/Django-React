1572da99452c9c51712ba171d353b0832970c972
Oana Belu <93271350+OanaBelu@users.noreply.github.com>
2024-11-20 13:40:48 +0200

    first commit

---
+++ frontend/src/components/LoadingIndicator.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/LoadingIndicator.css';
+
+const LoadingIndicator = () => {
+    return (
+        <div className="loading-indicator">
+            Loading...
+        </div>
+    );
+};
+
+export default LoadingIndicator;
